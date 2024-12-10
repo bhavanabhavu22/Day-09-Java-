a2a6cb04593df15f08c9782e4b916a64d030d598
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-10 12:23:30 +0530

    Add files via upload

---
+++ STRINGstatic.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class STRINGstatic
+{
+    public static void main(String args[])
+    {
+        String a="HEY GUYS";
+        String b=new String("WELCOME");
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
