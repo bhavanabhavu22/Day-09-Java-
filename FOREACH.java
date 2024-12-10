7a7a9bf46aa081c9ea1230a66fb463d33dbd7dfa
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-10 21:15:45 +0530

    Add files via upload

---
+++ FOREACH.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class FOREACH {
+    public static void main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+        String a[]={"APPLE","KIWI","DRAGON FRUIT"};
+        for(String b:a)
+        System.out.println(b+" ");
+    }
+    
+}
