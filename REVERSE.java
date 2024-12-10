b9754da894bb697e8a126d95af2630225c6f3e8c
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-10 21:16:05 +0530

    Add files via upload

---
+++ REVERSE.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class REVERSE 
+{
+
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+         String a=sc.nextLine();
+         String res="";
+         for(int i=0;i<a.length();i++)
+         {
+            res=a.charAt(i)+res;
+            System.out.println(res);
+         }
+    }
+}
+    
